aa75ea98996b769e443a3b308ee9f3f6e83ed51d
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-10 11:48:15 +0530

    initial commit

---
+++ Day_7/Flute.java
@@ -0,0 +1,12 @@
+public class Flute extends Instrument{
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    void play() {
+        System.out.println("Flute is playing toot toot toot toot");
+        
+    }
+    
+}
