f6ef85500372256c674a6a36c0fc10a32cbd4cf9
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-10 11:48:42 +0530

    initial commit

---
+++ Day_7/Guitar.java
@@ -0,0 +1,11 @@
+public class Guitar extends Instrument{
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    void play() {
+        System.out.println("Guitar is playing tin tin tin");
+    }
+    
+}
