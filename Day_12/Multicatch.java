4a51ad0279f04f2dad053d0836a487f99d466444
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-10 12:12:09 +0530

    initial commit

---
+++ Day_12/Multicatch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Multicatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Enter 2 numbers");
+       
+        try {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = a / b;
+            System.out.println("Division is " + c);
+
+        } catch (ArithmeticException e) {
+            System.out.println("can not divide by zero");
+        }catch(InputMismatchException e){
+            System.out.println("Plz enter int value");
+        }
+
+        System.out.println("Your Program is End");
+    }
+}
