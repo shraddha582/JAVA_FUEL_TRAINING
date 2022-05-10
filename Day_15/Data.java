d4ede50a17ab5466c7dc0bb1166f47caa6503dc1
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-10 12:43:09 +0530

    initial commit

---
+++ Day_15/Data.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Data{
+    public static void main(String[] args)throws Exception {
+      FileInputStream fin;
+      fin=new FileInputStream("D:\\Sudam\\Test3.txt");
+      DataInputStream din;
+      din=new DataInputStream(fin);
+      int x=din.readInt();
+      boolean y=din.readBoolean();
+      double z=din.readDouble();
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+      din.close();
+    }
+
+}
