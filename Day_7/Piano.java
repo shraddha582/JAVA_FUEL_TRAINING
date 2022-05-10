58b87d5b8ada3100a74c60edff3e5296ab8b0011
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-10 11:49:46 +0530

    initial commit

---
+++ Day_7/Piano.java
@@ -0,0 +1,11 @@
+public class Piano extends Instrument {
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    void play() {
+       System.out.println("Piano is playing tan tan tan tan");
+    }
+    
+}
