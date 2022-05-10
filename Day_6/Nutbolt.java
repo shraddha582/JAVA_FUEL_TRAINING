2a8034128da1c90868c6e774ac3c1dd99fa1a51c
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-10 11:34:34 +0530

    initial commit

---
+++ Day_6/Nutbolt.java
@@ -0,0 +1,23 @@
+class Nutbolt
+{
+ public static void main(String[] args)
+ {
+       int bolts;
+       int nuts;
+       int washer;
+       
+     Scanner ab=new Scanner(System.in)
+     System.out.println("Enter number of Bolts")
+     bolts=ab.next.Int();
+     System.out.println("Enter number of nuts")
+     nuts=ab.next.Int();
+     System.out.println("Enter number of washer")
+     bolts=ab.next.Int();
+     
+  }   
+}     
+     
+
+
+     
+       
