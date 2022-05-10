dd0c5dd5eb5a672d03ff13f2220a1863d3dddc9f
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-10 13:25:30 +0530

    initial commit

---
+++ Day_24/Demo.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        
+        Class.forName("org.postgresql.Driver");
+        System.out.println("Driver loaded");
+        Connection conn;
+        conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "abc");
+
+        System.out.println("Coonection done");
+
+        Statement stmt;
+        stmt=conn.createStatement();
+        String sql;
+        sql="insert into student values(700,'xyz')";
+        stmt.executeUpdate(sql);
+        System.out.println("record inserted");
+
+        conn.close();
+
+    }
+}
