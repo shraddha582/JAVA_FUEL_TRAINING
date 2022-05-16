e058306ce4ac81048ced1a99244fee5db3e13388
shraddha582 <75934321+shraddha582@users.noreply.github.com>
2022-05-16 14:40:29 +0530

    initial commit

---
+++ onetoonedemo/src/test/java/com/example/OnetoOneChoiceAssignmentdemo/OnetoOneChoiceAssignmentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnetoOneChoiceAssignmentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetoOneChoiceAssignmentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
